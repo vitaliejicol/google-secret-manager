a51007a69d7d7ddb6652f710503fbb39a123aa43
vitalie.jicol <63916548+vitaliejicol@users.noreply.github.com>
2023-03-26 22:05:32 -0500

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,10 @@
+# output "secret_names" {
+#   value = google_secret_manager_secret.secret.name
+#   description = "Secret name"
+# }
+
+# output "secret_versions" {
+#   value = google_secret_manager_secret_version.secret.name
+#   description = "The name list of Secret Versions"
+# }
+
