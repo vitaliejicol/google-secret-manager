3285ff4228a0b85389f42afebfcb9a50b3d85f9d
vitalie.jicol <63916548+vitaliejicol@users.noreply.github.com>
2023-04-03 00:20:13 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,9 @@
-output "secret_names" {
-  value = module.secret_manager.secret_names
-  description = "Secret name"
-}
-
-output "secret_versions" {
-  value = module.secret_manager.secret_versions
-  description = "The name list of Secret Versions"
-}
-
-output "roles" {
-  value       = module.secret_manager_iam.roles
-  description = "Roles which were assigned to members."
-}
-
-output "members" {
-  value       = module.secret_manager_iam.members
-  description = "Members which were bound to the Secret Manager Secrets."
+output "secrets_name_and_versions" {
+  description = "Secrets and their versions and member/roles"
+  value = {
+    secret_names             = module.secret_manager.secret_names
+    secret_versions          = module.secret_manager.secret_versions
+    secret_roles_and_members = module.secret_manager_iam
+  }
+  sensitive = false
 }
